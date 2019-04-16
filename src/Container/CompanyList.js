da52524e058a68141a56fd72fefa9cf39fb5b674
legrandgrand <alexandre.danty@gmail.com>
2019-04-16 16:50:46 +0200

    [Alex, Feature] Creating main pages for the project

---
+++ src/Container/CompanyList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CompanyDetail from '../Component/CompanyDetail';
+
+class ComputerList extends Component {
+    render() {
+      return (
+          <CompanyDetail/>
+  
+      );
+    }
+  }
+  
+  export default ComputerList;
+  
---
+++ src/Container/ComputerList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ComputerDetail from '../Component/ComputerDetail';
+
+class ComputerList extends Component {
+    render() {
+      return (
+          <ComputerDetail/>
+  
+      );
+    }
+  }
+  
+  export default ComputerList;
+  
