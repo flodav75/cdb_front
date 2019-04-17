75934d44ba18203026995e0dfa3944b775541dd8
legrandgrand <alexandre.danty@gmail.com>
2019-04-17 11:48:59 +0200

    [Feature] Implementing search

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 
-import './App.css';
 import ComputerList from './Container/ComputerList';
-import NavBar from './Component/NavBar';
+
 
 class App extends Component {
   render() {
     return (
-<div>
-      <NavBar/>
       <ComputerList/>
-      </div>
-
     );
   }
 }
