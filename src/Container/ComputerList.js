a54dda6f565cb07857961218c54e9f72193e1b1c
flodav75 <fdauvillier@excilys.com>
2019-04-23 16:36:48 +0200

    [feature] delete v2

--- src/Container/ComputerList.js
+++ src/Container/ComputerList.js
@@ -3,21 +3,30 @@ import ComputerDetail from '../Component/ComputerDetail';
 import {Table, Container, Row, Button} from 'reactstrap';
 import Search from "../Component/Search";
 import "../App.css";
-import Popup from "reactjs-popup";
-import DeletePopUp from "../Component/DeletePopUp";
-import {MOCK} from "../Mock";
 
 const address = 'http://10.0.1.70:8080/webapp/api/computers/'
 
 class ComputerList extends Component {
 
-    state={computers:MOCK,
-        isDelete: false
+    state={computers:[],
+        isDelete: false,
+        computersDelete:[]
     }
-
 
     componentDidMount() {
        this.getAll();
+    }
+
+
+    addComputerToDelete = (computer)=>{
+        this.setState({computersDelete:[...this.state.computersDelete, computer]});
+    }
+
+    removeComputerToDelete = (computer)=>{
+        var filtered = this.state.computersDelete.filter(function(value){
+            return value =! computer;
+        });
+        this.setState({computersDelete:filtered});
     }
 
     getAll(){
@@ -30,16 +39,26 @@ class ComputerList extends Component {
             .catch(error => console.log(error))
     }
 
+    clear=()=>{
+        this.setState({computersDelete:[]});
+    }
+
+    deleteComputers=()=>{
+        this.state.computersDelete.map(computer=>{
+            return this.delete(computer.id);
+        })
+        this.clear();
+        this.getAll();
+    }
 
     delete = (id)  => {
         fetch(address+`${id}`,
             {
                 method: "delete",
             }
-        ).then(()=>{this.getAll()})
+        )
     }
 
-    //Cross origin problem
     search = (name) => () => {
         fetch(address+'Search?name='+`${name}`)
             .then(result => {
@@ -56,6 +75,8 @@ class ComputerList extends Component {
                 <Container>
                     <Row>
                         <Search onSearch={this.search} />
+                        {this.state.computersDelete.length>0 && <Button variant="outline-warning" onClick={this.deleteComputers} >delete</Button>}
+                        {this.state.computersDelete.length>0 && <Button variant="outline-warning" onClick={this.clear} >clear</Button>}
                         <Table className="table">
                             <thead>
                                 <tr>
@@ -63,25 +84,23 @@ class ComputerList extends Component {
                                     <th>introduced</th>
                                     <th>discontinued</th>
                                     <th>company</th>
-                                    <th> delete</th>
+                                    <th>
+                                        {<Button variant="outline-warning" >Edit</Button>}
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {
                                     this.state.computers.map(computer => {
-                                        return <ComputerDetail key={computer.id} computer={computer} delete={this.delete}/>
+                                        return <ComputerDetail key={computer.id} computer={computer} removeToDelete={this.removeComputerToDelete} addToDelete={this.addComputerToDelete} delete={this.delete}/>
                                     })
                                 }
                             </tbody>
                         </Table>
                     </Row>
                     <Row>
-
-
-
                     </Row>
                 </Container>
-
             </div>
       );
     }
