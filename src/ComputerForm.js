c4d4ab7f10e801fd194cd6410cdb304c67f5e5c6
JakubSkorupinski96 <jskorupinski@excilys.com>
2019-04-26 10:18:57 +0200

    form fixes

--- src/ComputerForm.js
+++ src/ComputerForm.js
@@ -85,21 +85,44 @@ onChangeIntroduced = (event) => {
   this.setState({computer: {...this.state.computer, introduced: event.target.value}})
 };
 
-onChangeIntroduced3 = introDate => {
-  this.setState({ introDate },
-    {computer: {...this.state.computer, discontinued: introDate}})
-}
-
-onChangeIntroduced2 = (event) => {
-  this.setState({computer: {...this.state.computer, discontinued: event.target.value}})
-}
-
 onChangeDiscontinued = (event) => {
   this.setState({computer: {...this.state.computer, discontinued: event.target.value}})
 };
 
 onChangeCompany = (selected) => {
   this.setState({computer: {...this.state.computer, companyname: selected.label, companyId: selected.value}});
+}
+
+
+onValidate=()=>{
+  var regex= "^(([1-9])|(0[1-9])|(1[0-2]))-(([0-9])|([0-2][0-9])|(3[0-1]))-(([0-9][0-9])|([1-2][0,9][0-9][0-9]))$";
+  let correct=true;
+  let incorrect;
+
+
+  if (this.state.computer.name===""){
+      incorrect="\n name "
+      correct=false;
+  }
+  if (!regex.match(this.state.computer.introduced)){
+      incorrect=incorrect+"\n introduced date "
+      correct=false;
+  }
+
+  if (!regex.match(this.state.computer.introduced)){
+    incorrect=incorrect+"\n discontinued date "
+    correct=false;
+}
+
+  if(correct===true) {
+    this.state.UpdateMode
+      ? this.update(this.state.computer)
+      : this.add(this.state.computer)
+  }else{
+      alert("Le ou les champs: " + incorrect + "\nsont vides ou incorrects")
+  }
+
+
 }
 
 
@@ -112,21 +135,21 @@ render() {
   return (
     <Container>
       <Row>
-        <Col md={{size: 6, offset: 3}} >
+        <Col md={{size: 20}} >
     <Card>
     {this.state.UpdateMode ? <h1>Edit a computer</h1> : <h1>Add a computer</h1>}
     <CardBody>
     <Input placeholer="Computer name" value={computer && computer.name} onChange={this.onChangeName}/>
-    <Input placeholer="Introduced Date" value={computer && computer.introduced} onChange={this.onChangeIntroduced}/>
-    Introduced Date <input type="date"
+    Introduced Date: <input type="date"
           onChange={this.onChangeIntroduced}
           value={computer && computer.introduced}
         />
+    <Input placeholer="Introduced Date" value={computer && computer.introduced} onChange={this.onChangeIntroduced}/>
+    Discontinued Date: <input type="date"
+        onChange={this.onChangeDiscontinued}
+        value={computer && computer.discontinued}
+      />
     <Input placeholer="Discontinued Date" value={computer && computer.discontinued} onChange={this.onChangeDiscontinued}/>
-    Discontinued Date <input type="date"
-          onChange={this.onChangeDiscontinued}
-          value={computer && computer.discontinued}
-        />
     <Select options={dropdown} onChange={this.onChangeCompany}/>
     {this.state.UpdateMode
     ? <FontAwesomeIcon icon={faPen} onClick={this.update(this.state.computer)}/>
